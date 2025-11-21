84a8bbe8523b59b24e520dd690c683a57a5d6d7c
articulatebass <articulatebasstwo@gmail.com>
2025-11-21 13:43:12 -0500

    fixed build issue

--- build.js
+++ build.js
@@ -1,9 +1,10 @@
 const esbuild = require("esbuild");
 
 esbuild.build({
-  entryPoints: ["src/content/index.js"], // our content script entry
+  entryPoints: ["src/content/index.js"],
   bundle: true,
   outfile: "dist/content.js",
   sourcemap: true,
-  minify: false
+  minify: false,
+  format: "esm" // support import.meta.glob()
 }).catch(() => process.exit(1));
